b54bf928ca62c5e1638fd6e26ab0492154fe0998
lem0nbytes <158801334+lem0nmel0n@users.noreply.github.com>
2026-05-15 15:03:20 +1000

    Update endings.py

--- endings.py
+++ endings.py
@@ -1,146 +1,136 @@
-from .board import Board
-from random import randint, choice
-import os
+from connect4 import program, board, game
 import time
-import re
+import os
+import win, lose, tie
 
-class Game:
-	def __init__(self, players, rows=6, cols=7, save_file=None):
-		if len(players) < 2:
-			raise ValueError("At least two players are required.")
-		self.players = players
-		self.current_player_index = 0
-		self.rows = rows
-		self.cols = cols
-		self.board = Board(self.rows, self.cols)
-	def get_current_player(self):
-		return self.players[self.current_player_index]
+print("\033[1;34m")
 
-	def switch_player(self):
-		self.current_player_index = (self.current_player_index + 1) % len(self.players)
+def announce_event(title):
+	print(f">>{title.upper()}<<")
+def scene_header(room):
+	print(f"==={room.upper()}===")
+	print()
+def actionify(action):
+	print(f"[ {action.upper()} ]")
+	print()
+def goto_bedroom(): #asks you to go to your bedroom
+	print("go to bedroom? (yes/no) ")
+	bed = input("> ").lower()
+	print()
+	while "y" not in bed:
+		print("come on dude, don't be stubborn.")
+		print("go to bedroom? (yes/no) ")
+		bed = input("> ").lower()
+		print()
+def clear(): #clears the screen
+	if os.name == 'nt':
+		board = os.system('cls')
+	else:
+		board = os.system('clear')
 
-	def clear(self):
-		if os.name == 'nt':
-				board = os.system('cls')
-		else:
-				board = os.system('clear')
+def main():
+	clear()
+	scene_header("LAUNDRY ROOM")
+	print("""you are in the laundry. the laundry basket is overflowing and there are socks all over the place. your special striped ones are in the corner.
 
-	def villain_move(self):
-		villain_piece = self.players[1].piece
-		player_piece = self.players[0].piece
+a figure emerges from behind the laundry basket. 
+you instantly recognise his odd, villainous smile.
+it's dexter stinkman, eager to put his plans for world-domination into motion.
 
-		'''
-		these two blocks are for checking if the next move will be a 4 in a row
-		if the player will win next turn, it'll block
-		if the villain will win, it'll select that column
-		'''
-		for col in range(7):
-			temporary = self.board.copy()
-			if temporary.is_valid_move(col):
-				temporary.drop_piece(col, villain_piece)
-				if temporary.check_win(villain_piece):
-					return col
+he pulls out a connect 4 board, before placing it between the two of you.
+""")
 
-		for col in range(7):
-			temporary = self.board.copy()
-			if temporary.is_valid_move(col):
-				temporary.drop_piece(col, player_piece)
-				if temporary.check_win(player_piece):
-					return col
-		'''
-		these next two blocks of code are for checking 
-		if either the villain can make 3 in a row or 
-		if it can block the player from making 3 in a row. 
-		'''
-		for col in range(7):
-			temporary = self.board.copy()
-			if temporary.is_valid_move(col):
-				temporary.drop_piece(col, villain_piece)
-				if temporary.check_threes(villain_piece):
-					print("three!", col)
-					return col
+	announce_event("battle start")
+	print()
+	
+	print("dexter stinkman challenges you!")
+	print("are you ready? (yes/no) ")
+	ready = input("> ").lower()
+	while not ready.startswith("y"):
+		print()
+		print("fine, we'll wait.")
+		print("are you ready yet? (yes/no) ")
+		ready = input("> ").lower()
+		
 
-		for col in range(7):
-			temporary = self.board.copy()
-			if temporary.is_valid_move(col):
-				temporary.drop_piece(col, player_piece)
-				if temporary.check_threes(player_piece):
-					print("blocked!", col)
-					return col
+	result = program.run() #runs the connect 4 program
+	time.sleep(1)
+	
+	clear()
 
-		col_list = [0,1,2,2,3,3,3,3,4,4,5,6] #favouring the middle columns over the edge columns
+	#result = "win" #i use this for testing the different endings
 
-		for col in range(7): # for every column
-			temporary = self.board.copy() #create a board copy
-			if temporary.is_valid_move(col): 
-				temporary.drop_piece(col, villain_piece) # the villain drops a piece 
+	if result == "win": #you win
+		actionify("victory")
+		time.sleep(1)
+		print("after vanquishing arch-nemesis dexter stinkman at a gruelling game of connect 4, the world was finally safe. he retreated into the shadows, distraught at even the *thought* of defeat.")
+		print()
+		goto_bedroom()
 
-				for col2 in range(7): #for every column [again]
-					if temporary.is_valid_move(col2):
-						temporary.drop_piece(col2, player_piece) #drop the player piece
-						if temporary.check_win(player_piece): 
-							col_list = [column for column in col_list if column != col2] #the columns that dont allow the player an instant win
+	elif result == "tie": #you tied
+		actionify("tie")
+		time.sleep(1)
+		print("looks like you tied with dexter. chose your next move wisely.")
+		print("[1] punch him")
+		print("[2] become best friends")
+		print("[3] scream")
+		next_move = input("\n> ").lower()
+		print()
+		while next_move not in "123":
+			print("invalid answer. try again dude.")
+			next_move = input("> ").lower()
+			print()
+		if next_move == "1": #you punch dexter stinkman
+			actionify("punch")
+			print("you stare at the villain. he stares back.\n")
+			print("your knuckles protrude from your clenched fist before you swing at him.")
+			print("somehow, dexter manages to get hit by the sloppy punch.")
+			print("he slips on your lucky socks before tumbling out the window!")
+			print()
+			print("he lands in a bush with a hard thump, completely stuck.")
+			print()
+		elif next_move == "2": #you try to befriend stinkman
+			actionify("attempt to become friends")
+			print("in case you didn't notice, dexter is a lonely, non-showering, villain who hates people. he escapes through the window while you cry about not having friends.")
+			print()
+			print("that was never gonna work out, man.")
+		elif next_move == "3": #you scream [with catastrophic consequences]
+			actionify("scream")
+			print("your throat is on fire as you scream.\n")
+			print('"SHUT IT!!" he screams back. ')
+			print("you see him panicking and picking up a conveniently placed baseball bat. ")
+			print("\n...\n")
+			print('you are suddenly and violently dead.')
+			print("someone should have warned you about that.")
+			print()
+			quit()
+		print("---")
+		print()
+		print("maybe just try to sleep off the disappointment.")
+		goto_bedroom()
 
-		col = choice(col_list)
-		while not self.board.is_valid_move(col):
-			col = choice(col_list)
-		return col
+	else: #you lost
+		actionify("defeat") 
+		time.sleep(1)
+		print("YOU LOST.")
+		print("your connect 4 tutor sighs disappointedly through the window. \nyou can't believe it.")
+		print("you lost. badly.")
+		print()
+		print("before you could even process your defeat, he vanished into the shadows, cackling. his plans for world domination are already in motion. ")
+		print("and it's all. your. fault.")
+		print()
+		print("---")
+		print()
+		print("oh well. you're tired.")
+		goto_bedroom()
 
-
-	def play(self):
-		self.clear()
-		self.board.print_board()
-		while True:
-			current_player = self.get_current_player()
-			print(f"{current_player.name} turn.")
-			if "your" in current_player.name:
-				col = input("enter the column (0-6) to drop your piece: ")
-				if not col:
-					print()
-					print("do you want to exit?")
-					leave = input("> ").lower()
-					print()
-					if leave.startswith("y"):
-						print("it was sad you couldn't stay.")
-						exit()
-				col = re.sub(r"\D", "", col)
-
-				while not col.isnumeric():
-					print()
-					print("try entering a number.")
-					print()
-					col = input("enter the column (0-6) to drop your piece: ")
-					col = re.sub(r"\D", "", col)
-				col = int(col)
-
-				if col > 6: col = 6
-				while not self.board.is_valid_move(col):
-					print("that ain't a valid move, dude. try again.")
-					col = int(input("enter the column (0-6) to drop your piece: "))
-					if col > 6: col = 6
-
-				self.board.drop_piece(col, self.get_current_player().piece)
-			else:
-				col = self.villain_move()
-				self.board.drop_piece(col, self.get_current_player().piece)
-				time.sleep(1.5)
-
-			self.clear()
-			self.board.print_board()
-
-			if self.board.check_win(self.get_current_player().piece):
-				time.sleep(1)
-				self.clear()
-				self.board.print_board()
-				time.sleep(1)
-				if "your" in self.get_current_player().name:
-					return "win"
-				else:
-					time.sleep(1)
-					return "lose"
-			elif self.board.is_full():
-				return "tie"
-
-
-
-			self.switch_player()
+	clear()
+	print()
+	#this code block calls the main functions of the win, lose and tie programs
+	announce_event("ending")
+	if result == "win":
+		win.main()
+	elif result == "tie":
+		tie.main(next_move)
+	else:
+		lose.main()
